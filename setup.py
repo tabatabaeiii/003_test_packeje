8bca282d809719fa3be806a7e36f95e95e574f54
MT.ali <mr.tabatabaeiii@gmail.com>
2023-08-27 15:49:51 +0430

    test01

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fixer-demo',
+    version='0.1',
+    description='Salam kardan b gambo ha',
+    url='#',
+    author='MT.ali',
+    author_email='mr.tabatabaeiii@gmail.com',
+    license='MIT',
+    packages=['fixer'],
+    zip_safe=False
+)
